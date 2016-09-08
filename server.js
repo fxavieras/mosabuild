52ad37542c8238fb508636a48e9031324238d7e9
Felix Amaya-Spornitz <felixamayaspornitz@gmail.com>
2016-09-08 15:55:26 -0500

    Committing changes to mosabuild project. Updates to server.js for readability and redundancies.

--- server.js
+++ server.js
@@ -23,16 +23,13 @@ var db = require('./config/db');
 
 // set our port
 var port = process.env.PORT || 8080;
+app.set('superSecret', config.secret); // secret variable
 
-// get all data/stuff of the body (POST) parameters
-// app.post('/loginservice',loginservice.test);
-// parse application/json
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
 app.use(bodyParser.json());
-
-// parse application/vnd.api+json as json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
@@ -45,7 +42,6 @@ app.use(express.static(__dirname + '/public'));
 require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
-// startup our app at http://localhost:8080
 app.listen(port);
 
 // shoutout to the user
@@ -61,21 +57,4 @@ app.post('/LoginService', bodyParser(), function(req, res) {
     window.location.replace("/views/timeline.html");
 });
 
-var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-app.set('superSecret', config.secret); // secret variable
-
-// use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// use morgan to log requests to the console
-app.use(morgan('dev'));
-
-// =======================
-// routes ================
-// =======================
-// basic route
-app.get('/', function(req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
 
